032b973009a589274dc72c9a9f055911fea06224
Spencer Reyka <spencer.reyka@gmail.com>
2018-07-20 00:38:20 -0400

    added mockito and junit, got them working in the build.gradle. Made hello world test files for JUnit to make sure it was working. Tested out sucess and failure.

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class HelloWorld {
+
+    public static void main(){
+        System.out.println();
+    }
+    public String printHello(){
+        return "Hello, World!";
+    }
+}
---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+
+public class HelloWorldTest {
+
+    @Test
+    @DisplayName("Testing Hello World!")
+    public void HelloWorldTest(){
+        HelloWorld helloWorld = new HelloWorld();
+        Assertions.assertEquals(helloWorld.printHello(),"Hello, World!");
+    }
+}
